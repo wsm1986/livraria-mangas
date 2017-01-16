f9b5b1c2b4fe69b1fd668503abdcd0c1177d31e4
Wellington Moreira <dbaldani@uoldiveo.com>
2017-01-16 16:53:18 -0200

    Prod

--- wine/src/main/java/com/livraria/config/WebConfig.java
+++ wine/src/main/java/com/livraria/config/WebConfig.java
@@ -5,29 +5,38 @@ import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
-		return (container ->
-		container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
-							new ErrorPage(HttpStatus.FORBIDDEN, "/403")));
+		return (container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
+				new ErrorPage(HttpStatus.FORBIDDEN, "/403")));
 	}
-	
-	/*@Bean
-	public DomainClassConverter<FormattingConversionService> domainClassConverter(
-			FormattingConversionService conversionService) {
-		return new DomainClassConverter<FormattingConversionService>(conversionService);
-	}*/
-	
+
+	/*
+	 * @Bean public DomainClassConverter<FormattingConversionService>
+	 * domainClassConverter( FormattingConversionService conversionService) {
+	 * return new
+	 * DomainClassConverter<FormattingConversionService>(conversionService); }
+	 */
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addRedirectViewController("/", "/livro/novo");
 	}
-	
-}
 
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("templates/");
+		// resolver.setSuffix(".html");
+		return resolver;
+	}
+
+}
