ac40c829cc97f70064ab6443f9d94a5ec76d939a
Wellington Moreira <dbaldani@uoldiveo.com>
2017-01-16 17:32:09 -0200

    Prod

--- wine/src/main/java/com/livraria/config/WebConfig.java
+++ wine/src/main/java/com/livraria/config/WebConfig.java
@@ -44,7 +44,7 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		// resource resolution infrastructure, which is highly recommended.
 		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
 		templateResolver.setApplicationContext(this.applicationContext);
-		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setPrefix("/templates/");
 		templateResolver.setSuffix(".html");
 		// HTML is the default value, added here for the sake of clarity.
 		// Template cache is true by default. Set to false if you want
