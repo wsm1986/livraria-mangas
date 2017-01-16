80a2085a8a7acdea21f0fa5cce29bc0a0faeb04c
Wellington Moreira <dbaldani@uoldiveo.com>
2017-01-16 18:55:35 -0200

    najara

--- wine/src/main/java/com/livraria/config/WebConfig.java
+++ wine/src/main/java/com/livraria/config/WebConfig.java
@@ -1,66 +1,45 @@
 package com.livraria.config;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-
-
-	/*
-	 * @Bean public DomainClassConverter<FormattingConversionService>
-	 * domainClassConverter( FormattingConversionService conversionService) {
-	 * return new
-	 * DomainClassConverter<FormattingConversionService>(conversionService); }
-	 */
-
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return (container ->
+		container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
+							new ErrorPage(HttpStatus.FORBIDDEN, "/403")));
+	}
+	
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolve() {
+		InternalResourceViewResolver resolve = new InternalResourceViewResolver();
+		resolve.setPrefix("/templates/");
+		resolve.setSuffix(".html");
+		resolve.setExposedContextBeanNames("livraria-mangas"); // Disponibiliza
+																// o Bean para o
+																// jsp
+		return resolve;
+	}
+	
+	/*@Bean
+	public DomainClassConverter<FormattingConversionService> domainClassConverter(
+			FormattingConversionService conversionService) {
+		return new DomainClassConverter<FormattingConversionService>(conversionService);
+	}*/
+	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addRedirectViewController("/", "/livro/novo");
 	}
-
-	@Bean
-	public SpringResourceTemplateResolver templateResolver() {
-		System.out.println("PASSOU AQUI ");
-		// SpringResourceTemplateResolver automatically integrates with Spring's
-		// own
-		// resource resolution infrastructure, which is highly recommended.
-		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-		templateResolver.setPrefix("/templates/");
-		templateResolver.setSuffix(".html");
-		// HTML is the default value, added here for the sake of clarity.
-		// Template cache is true by default. Set to false if you want
-		// templates to be automatically updated when modified.
-		templateResolver.setCacheable(true);
-		return templateResolver;
-	}
-
-	@Bean
-	public SpringTemplateEngine templateEngine() {
-		// SpringTemplateEngine automatically applies SpringStandardDialect and
-		// enables Spring's own MessageSource message resolution mechanisms.
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver());
-		// Enabling the SpringEL compiler with Spring 4.2.4 or newer can
-		// speed up execution in most scenarios, but might be incompatible
-		// with specific cases when expressions in one template are reused
-		// across different data types, so this flag is "false" by default
-		// for safer backwards compatibility.
-		return templateEngine;
-	}
-
-	@Bean
-	public ThymeleafViewResolver viewResolver() {
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setTemplateEngine(templateEngine());
-		return viewResolver;
-	}
-
-
 }
+
