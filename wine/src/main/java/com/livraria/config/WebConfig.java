e68ced80b4d1cd7aede55b42ed4bbbccab7d03d0
Wellington Moreira <dbaldani@uoldiveo.com>
2017-01-17 13:01:23 -0200

    prod

--- wine/src/main/java/com/livraria/config/WebConfig.java
+++ wine/src/main/java/com/livraria/config/WebConfig.java
@@ -5,12 +5,9 @@ import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
@@ -21,16 +18,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 							new ErrorPage(HttpStatus.FORBIDDEN, "/403")));
 	}
 	
-	
-	@Bean
-	public InternalResourceViewResolver internalResourceViewResolve() {
-		InternalResourceViewResolver resolve = new InternalResourceViewResolver();
-		resolve.setPrefix("classpath:/templates");
-		resolve.setSuffix(".html");
-		resolve.setExposedContextBeanNames("livraria-mangas"); 
-		return resolve;
-	}
-	
+
 	/*@Bean
 	public DomainClassConverter<FormattingConversionService> domainClassConverter(
 			FormattingConversionService conversionService) {
