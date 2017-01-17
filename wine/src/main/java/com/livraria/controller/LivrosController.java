8236fdb984f6c8b68ad43e5e798cf5707f48829b
wsm1986@gmail.com <wsm1986@gmail.com>
2017-01-16 23:19:17 -0200

    prod

--- wine/src/main/java/com/livraria/controller/LivrosController.java
+++ wine/src/main/java/com/livraria/controller/LivrosController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,10 +29,9 @@ public class LivrosController {
 	private CadastroLivroService cadastroVinhoService;
 	
 	@RequestMapping
-	public ModelAndView pesquisa() {
-		ModelAndView mv = new ModelAndView("/livros/ListaLivro");
-		mv.addObject("livros", livro.findAll());
-		return mv;
+	public String pesquisa(Model mv) {
+		mv.addAttribute("livros", livro.findAll());
+		return "/livros/ListaLivro";
 	}
 	
 	@RequestMapping("/novo")
